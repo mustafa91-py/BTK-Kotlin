a90cd107abf7fdee77840e941d3a1c4ae4e14629
Mustafa2 <Mustafauyar91@gmail.com>
2024-09-01 12:33:26 +0300

    numbers

--- app/src/main/java/com/example/kotlinbaslangic/Variable.kt
+++ app/src/main/java/com/example/kotlinbaslangic/Variable.kt
@@ -1,13 +1,36 @@
 package com.example.kotlinbaslangic
 
+import kotlin.reflect.typeOf
+
 class Variable {
     init {
         var name = "mustafa UYAR"
-        println(square(7))
-    }
-    private fun square(number: Int) = number * number
-}
+        tamsayi()
 
+        //explicit
+        var x:Long = 7 // burdaki x ile diger x farkli katmdanlar
+
+        floating()
+
+
+    }
+    private fun tamsayi(){
+        println("tam sayi")
+
+
+        //implicit
+        var x = 5
+
+    }
+    private fun floating(){
+        var x = 5.5f //f koyunca float olur
+        var y = 5.5 //double olur
+        println("$x ${x::class}")
+        println(y)
+
+    }
+
+}
 fun main(){
    Variable()
 }
