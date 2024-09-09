e3ec96e0b14b73f1b2da7fcdb620f72e22c71f53
Mustafa2 <Mustafauyar91@gmail.com>
2024-09-09 07:57:32 +0300

    boolen

--- app/src/main/java/com/example/kotlinbaslangic/Variable.kt
+++ app/src/main/java/com/example/kotlinbaslangic/Variable.kt
@@ -5,13 +5,16 @@ import kotlin.reflect.typeOf
 class Variable {
     init {
         var name = "mustafa UYAR"
-        tamsayi()
 
         //explicit
         var x:Long = 7 // burdaki x ile diger x farkli katmdanlar
 
-        floating()
 
+//        tamsayi()
+
+//        floating()
+
+//        string()
 
     }
     private fun tamsayi(){
@@ -27,10 +30,57 @@ class Variable {
         var y = 5.5 //double olur
         println("$x ${x::class}")
         println(y)
+        var z:UInt = 5u//negatif degerler olmaz
+        println(z)
+
+    }
+    private fun string(){
+        val name = "mustafa"
+        val surname = "uyar"
+        println(name.uppercase())
+        println(name)
+        println(name + surname.uppercase())
+
+        val age = "28"
+        val year = "2024"
+        println(age + year)
+
+        val benimStr:String
+        benimStr = "initialize" // initialize,init,initialization,baslatma
+        println(benimStr)
+
+        //Conversion
+
+        println(year.toInt() - age.toInt())
+
+
+    }
+    fun boolean(){
+        println("ders 3_8")
+
+        fun x(): Int{
+            println("x")
+            return 5
+        }
+
+        fun y(): Int{
+            println("y")
+            return 2
+        }
+
+        fun z(): Int{
+            println("z")
+            return 10
+        }
+        // julia da ki kisa devre mantigi gibi
+        //Short-circuit evaluation
+        println(x()<y() && x()>z()) // kısa devre var ilk kosul yanlis ise digerlerine bakilmiyor && ve de
+        println(x()>y() || x()>z()) // kısa devre var ilk kosul yanlis ise digerlerine bakilmiyor && ve de
 
     }
 
 }
 fun main(){
-   Variable()
+    val variable = Variable()
+    variable.boolean()
 }
